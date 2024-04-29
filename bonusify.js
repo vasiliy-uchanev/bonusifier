23df2625563285b0c1bdc04de6742db6645028a8
Vasiliy Uchanev <vasiliy.uchanev@servicetitan.com>
2024-04-29 13:48:57 -0700

    cleaning up, adding an "are you ready.." prompt

--- bonusify.js
+++ bonusify.js
@@ -1,34 +1,41 @@
 import fetch from 'node-fetch'
 import fs from 'fs'
 import YAML from 'yaml'
+import readline from 'readline'
 
-// const emailEncoded = encodeURIComponent(email);
-// const userResponse = await fetch(`https://bonus.ly/api/v1/users?limit=20&email=${emailEncoded}&include_archived=false&${tokenQuery}`);
-// const data = await userResponse.json();
-// console.log(data);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
-// const givenBonusesResponse = await fetch(`https://bonus.ly/api/v1/bonuses?limit=20&giver_email=${emailEncoded}&include_children=false&${tokenQuery}`);
-// const lastBonuses = await givenBonusesResponse.json();
+function readConfig() {
+    const configFile = fs.readFileSync('./config.yaml', 'utf8')
+    const config = YAML.parse(configFile)
+
+    const token = config.token
+    const tokenQuery = `access_token=${token}`
+    const recipients = config.team
+    const userId = config.userId
+    return { tokenQuery, recipients, userId }
+}
+
+async function getCurrentBalance(userId, tokenQuery) {
+    console.log(userId, tokenQuery)
+    const getUserResponse = await fetch(
+        `https://bonus.ly/api/v1/users/${userId}?${tokenQuery}`
+    )
+    const userData = await getUserResponse.json()
+    const givingBalance = userData.result.giving_balance
+    return givingBalance
+}
 
 console.log('starting up...')
 
 console.log('reading config...')
-
-const configFile = fs.readFileSync('./config.yaml', 'utf8')
-const config = YAML.parse(configFile)
-
-const token = config.token
-const tokenQuery = `access_token=${token}`
-
-const recipients = config.team
-const userId = config.userId
+const { tokenQuery, recipients, userId } = readConfig()
 
 console.log('getting user data...')
-const getUserResponse = await fetch(
-    `https://bonus.ly/api/v1/users/${userId}?${tokenQuery}`
-)
-const userData = await getUserResponse.json()
-const givingBalance = userData.result.giving_balance
+const givingBalance = await getCurrentBalance(userId, tokenQuery)
 console.log(`giving balance: ${givingBalance}`)
 
 console.log(`calculating bonus amount...`)
@@ -40,22 +47,32 @@ console.log(`remainder: ${remainder}`)
 const message = `${recipients
     .map((x) => `@${x}`)
     .join(' ')} +${amount} Thank you all! #dreamteam`
-console.log(`message: ${message}`)
+console.log('------------------------------------------------------------')
+console.log('MESSAGE:')
+console.log(`${message}`)
+console.log('------------------------------------------------------------')
 
-const request = {
-    reason: message,
-}
-
-console.log('sending bonus...')
-const sendBonusResponse = await fetch(
-    `https://bonus.ly/api/v1/bonuses?${tokenQuery}`,
-    {
-        method: 'post',
-        body: JSON.stringify(request),
-        headers: { 'Content-Type': 'application/json' },
+rl.question('Are you ready to send the bonus? (y/n)', async (answer) => {
+    if (answer === 'y') {
+        console.log('sending bonus...')
+        const request = {
+            reason: message,
+        }
+        const sendBonusResponse = await fetch(
+            `https://bonus.ly/api/v1/bonuses?${tokenQuery}`,
+            {
+                method: 'post',
+                body: JSON.stringify(request),
+                headers: { 'Content-Type': 'application/json' },
+            }
+        )
+        const data = await sendBonusResponse.json()
+        //todo: parse response and check for errors
+        console.log(
+            `bonus sent. ${recipients.length} people received ${amount} each.`
+        )
+        console.log(data)
     }
-)
-const data = await sendBonusResponse.json()
-console.log(data)
-
-console.log('done.')
+    console.log('done.')
+    rl.close()
+})
